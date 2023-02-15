1ad046ab994b6f4a551ca3dcb98371ab8f50c5c9
Viking7813 <lucas.jonsson@elev.ga.lbs.se>
2023-02-15 15:13:20 +0100

    Sluta va sus

--- Assets/Springs.cs
+++ Assets/Springs.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Springs : MonoBehaviour
 {
-    private Rigidbody2D _body;
+    public Rigidbody2D _body;
     public float SpringJump = 20.0f;
     private void Start()
     {
-        _body = gameObject.GetComponent<Rigidbody2D>();
+        GameObject.Find("Bengt").GetComponent<Rigidbody2D>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.tag == "Player")
         {
-            _body.AddForce(Vector3.up * SpringJump, ForceMode2D.Impulse);
+            _body.AddForce(Vector3.up * SpringJump);
         }
     }
 }
